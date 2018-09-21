564e0139b2f37544a72c4793d088d5ec2ac0e963
mklipe <mklipe@yahoo.com.br>
2018-09-21 08:07:47 -0400

    Validacao com Bean Validation

--- src/main/java/com/mklipe/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/mklipe/cursomc/dto/CategoriaDTO.java
@@ -2,20 +2,30 @@ package com.mklipe.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.mklipe.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
 	
 	public CategoriaDTO(Categoria c) {
 		this.id = c.getId();
 		this.nome = c.getNome();
 	}
 	
-
 	public Integer getId() {
 		return id;
 	}
--- src/main/java/com/mklipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/mklipe/cursomc/services/CategoriaService.java
@@ -67,7 +67,9 @@ public class CategoriaService {
 		
 	}
 	
-	
+	public Categoria fromDTO(CategoriaDTO objDTO){
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
 	
 	
 	
