3a9d92ca7b72d8102d347cf633755752196e649a
mklipe <mklipe@yahoo.com.br>
2018-09-21 07:44:07 -0400

    Adicionando paginação de Categorias

--- src/main/java/com/mklipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/mklipe/cursomc/services/CategoriaService.java
@@ -1,12 +1,14 @@
 package com.mklipe.cursomc.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mklipe.cursomc.domain.Categoria;
@@ -57,5 +59,17 @@ public class CategoriaService {
 			
 		return listaDTO;
 	} 
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	
+	
+	
+	
 	
 }
