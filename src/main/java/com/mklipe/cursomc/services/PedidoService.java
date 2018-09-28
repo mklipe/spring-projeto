945cc64b52c2a758d4d0a32ddeedb59f8ea11899
mklipe <mklipe@yahoo.com.br>
2018-09-28 14:59:46 -0400

    Adicionando toString de Pedido

--- src/main/java/com/mklipe/cursomc/services/PedidoService.java
+++ src/main/java/com/mklipe/cursomc/services/PedidoService.java
@@ -31,6 +31,9 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	
 	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido find(Integer id) {
@@ -46,7 +49,7 @@ public class PedidoService {
 		obj.setInstante(new Date());
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
@@ -56,12 +59,15 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		
 		for (ItemPedido ip : obj.getItens()) {
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
 		
 		return obj;
 	} 
