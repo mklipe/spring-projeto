f06b98111b604713996b766f3254fe6a105b4823
mklipe <mklipe@yahoo.com.br>
2018-09-28 03:54:37 -0400

    Inserindo novo Cliente com POST

--- src/main/java/com/mklipe/cursomc/resources/ClienteResource.java
+++ src/main/java/com/mklipe/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.mklipe.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mklipe.cursomc.domain.Cliente;
 import com.mklipe.cursomc.dto.ClienteDTO;
+import com.mklipe.cursomc.dto.ClienteNewDTO;
 import com.mklipe.cursomc.services.ClienteService;
 
 @RestController
@@ -31,6 +34,15 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/mklipe/cursomc/services/ClienteService.java
+++ src/main/java/com/mklipe/cursomc/services/ClienteService.java
@@ -4,16 +4,24 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mklipe.cursomc.domain.Cidade;
 import com.mklipe.cursomc.domain.Cliente;
+import com.mklipe.cursomc.domain.Endereco;
+import com.mklipe.cursomc.domain.enums.TipoCliente;
 import com.mklipe.cursomc.dto.ClienteDTO;
+import com.mklipe.cursomc.dto.ClienteNewDTO;
 import com.mklipe.cursomc.repositories.ClienteRepository;
+import com.mklipe.cursomc.repositories.EnderecoRepository;
 import com.mklipe.cursomc.services.exceptions.DataIntegrityException;
 import com.mklipe.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,19 +29,26 @@ import com.mklipe.cursomc.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
+		Optional<Cliente> obj = clienteRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));	
 	} 
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = clienteRepository.save(obj);
+		
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -41,8 +56,7 @@ public class ClienteService {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		
-		
-		return repo.save(newObj);
+		return clienteRepository.save(newObj);
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
@@ -54,7 +68,7 @@ public class ClienteService {
 		find(id);
 		
 		try {	
-			repo.deleteById(id);
+			clienteRepository.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma Cliente porque há entidades relacionadas.");
@@ -62,7 +76,7 @@ public class ClienteService {
 	}
 
 	public List<ClienteDTO> findAll() {
-		List<Cliente> lista = repo.findAll();
+		List<Cliente> lista = clienteRepository.findAll();
 		List<ClienteDTO> listaDTO = 
 				lista.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 			
@@ -72,12 +86,36 @@ public class ClienteService {
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageRequest);
+		return clienteRepository.findAll(pageRequest);
 		
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDTO){
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());
+	}
+
+	public Cliente fromDTO(@Valid ClienteNewDTO objDTO) {
+		Cliente cli = 
+				new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), 
+				objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		
+		if (objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
+		
 	}
 	
 }
