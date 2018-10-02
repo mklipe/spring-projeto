fb126a81981900687760aa00d63a395d940b30d8
mklipe <mklipe@yahoo.com.br>
2018-10-02 02:19:16 -0400

    Esqueci a senha

--- src/main/java/com/mklipe/cursomc/services/EmailService.java
+++ src/main/java/com/mklipe/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.mklipe.cursomc.domain.Cliente;
 import com.mklipe.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -14,5 +15,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
