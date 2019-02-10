7c43eab98563c812a37360c0983329051dc850f8
mklipe <mklipe@yahoo.com.br>
2019-02-09 21:06:14 -0300

    Buscando usuário por email

--- src/main/java/com/mklipe/cursomc/services/ClienteService.java
+++ src/main/java/com/mklipe/cursomc/services/ClienteService.java
@@ -113,6 +113,23 @@ public class ClienteService {
 		return listaDTO;
 	} 
 	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(email);
+		
+		if (cliente == null ) throw new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		
+		
+		return cliente;
+		
+	}
+	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
